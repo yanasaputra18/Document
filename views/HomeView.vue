cf420ea4fbbe522c212446ac954be768ca55df64
yanasaputra18 <yanasaputra152@gmail.com>
2024-05-25 21:09:58 +0700

    first commit

---
+++ views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home"></div>
+  <NavbarMain />
+</template>
+
+<script>
+// @ is an alias to /src
+import NavbarMain from "@/components/NavbarMain.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    NavbarMain,
+  },
+};
+</script>
